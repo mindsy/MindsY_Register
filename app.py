4b3f4261979b77df6999d241044cccaa9142ecf5
Daniel Sousa <daniel_sousa.unb@outlook.com>
2019-04-11 19:26:25 -0300

    Adicionando tabelas referente a pessoas

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.congig['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/daniel/Documents/MDS/SignIn/mindsy.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-  return 'Hello World'
 
-if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    telephone = db.relationship('telephone', backref='owner1')
+    natural_person = db.relationship('natural_person', backref='owner2')
+    legal_person = db.relationship('legal_person', backref='owner3')
+
+
+class Telephone(db.Model):
+    number = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    owner1_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+
+
+class NaturalPerson(db.Model):
+    cpf = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    owner2_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    psychologist = db.relationship('psychologist', backref='owner4')
+
+
+class Psychologist(db.Model):
+    crp = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    owner4_id = db.Column(db.Integer, db.ForeignKey('legal_person.id'))
+
+
+class LegalPerson(db.person):
+    cnpj = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    owner3_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+
