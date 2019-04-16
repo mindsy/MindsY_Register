4343857813e64bcc85c5269b8b1aff31461f0bbd
Daniel Sousa <daniel_sousa.unb@outlook.com>
2019-04-16 20:10:31 -0300

    Ajustando tabelas referentes ao psicológo

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ class Person(db.Model):
     name = db.Column(db.String)
     email = db.Column(db.String)
     cpf = db.Column(db.String(11))
-    telephone = db.relationship('Telephone', backref='tel_person', Lazy='dynamic', uselist=True)
-    hospital = db.relationship('Hospital', backref='hospital_person', Lazy='dynamic', uselist=False)
-    psychologist = db.relationship('Psychologist', backref='person_psy', Lazy='dynamic', uselist=False)
+    telephone = db.relationship('Telephone', backref='tel_person', lazy='dynamic', uselist=True)
+    hospital = db.relationship('Hospital', backref='hospital_person', lazy='dynamic', uselist=False)
+    psychologist = db.relationship('Psychologist', backref='person_psy', lazy='dynamic', uselist=False)
 
 
 class Telephone(db.Model):
@@ -31,18 +31,25 @@ class Hospital(db.Model):
     cnpj = db.Column(db.String, primary_key=True, autoincrement=False)
     social_reason = db.Column(db.String)
     hospital_person_id = db.Column(db.Integer, db.ForeignKey('person.id'), unique=True)
-    hospital = db.relationship('HospitalPsychologist', backref='hospital', Lazy='dynamic', uselist=True)
+    hospital = db.relationship('HospitalPsychologist', backref='hospital', lazy='dynamic', uselist=True)
 
 
 class Psychologist(db.Model):
-    crp = db.Column(db.String(128), primary_key=True, autoincrement=False)
-    password = db.Column(db.String(128))
+    crp = db.Column(db.String, primary_key=True, autoincrement=False)
+    password = db.Column(db.String)
+    date_of_birth = db.Column(db.String)
     person_psy_id = db.Column(db.Integer, db.ForeignKey('person.id'), unique=True)
     hospital_psychologist = db.relationship('HospitalPsychologist', backref='crp_psychologist',
-                                            Lazy='dynamic', uselist=True)
+                                            lazy='dynamic', uselist=True)
 
 
 class HospitalPsychologist(db.Model):
     initial_date = db.Column(db.String)
-    crp_psychologist_crp = db.Column(db.Integer, db.ForeignKey('psychologist.crp'), unique=False)
-    hospital_cnpj = db.Column(db.Integer, db.ForeignKey('hospital.cnpj'), unique=False)
+    crp_psychologist_crp = db.Column(db.Integer, db.ForeignKey('psychologist.crp'), unique=False,
+                                     primary_key=True, autoincrement=False)
+    hospital_cnpj = db.Column(db.Integer, db.ForeignKey('hospital.cnpj'), unique=False,
+                              primary_key=True, autoincrement=False)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
