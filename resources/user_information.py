6b7c9ca02f924582317b186f443006631f38f9eb
Daniel Sousa <daniel_sousa.unb@outlook.com>
2019-05-14 00:17:53 -0300

    Corrigindo metodos GET e EDIT
    Co-authored-by: MatheusFilipe95 <mafieu666@hotmail.com>
    Co-authored-by: luderibeiro <ludeyuri07@gmail.com>
    Co-authored-by: pAmanda <amandoca_779@hotmail.com>

--- resources/user_information.py
+++ resources/user_information.py
@@ -1,48 +1,20 @@
 from flask_restful import Resource, reqparse, request
 from flask_jwt_extended import jwt_required
-from models.person import PersonModel
-from models.telephone import TelephoneModel
 from models.psychologist import PsychologistModel
-from models.hospital import HospitalModel
-from models.psychologist_hospital import PsychologistHospitalModel
+from models.person import PersonModel
 
 
 class ShowInformationUserID(Resource):
     @jwt_required
-    def get(self, id):
-        person = PersonModel.find_by_id(id)
-        if person:
-            person_info = person.json()
-            crp = person.psychologists.crp
-            date_of_birth = person.psychologists.date_of_birth
-            password = person.psychologists.password
+    def get(self, crp):
+        psychologists = PsychologistModel.find_by_crp(crp)
+        if psychologists:
+            person_info = psychologists.person_psy.json()
+            psychologists_info = psychologists.json()
+            hospital_info = psychologists.hospital_psychologists[0].hospital.json()
 
-            hospital = PersonModel.find_by_id(2)
-            registry_number = hospital.hospitals.registry_number
-            social_reason = hospital.hospitals.social_reason
-
-            output = {'Basic Informations': [person_info], 'Psychologist Information': {'crp': crp, 'date of birth': date_of_birth, 'password': password},
-            'Hospital': {'social_reason': social_reason, 'registry_number': registry_number}}
+            output = {'Basic Informations': [person_info], 'Psychologist Information': [psychologists_info],
+            'Hospital': hospital_info}
 
             return {'User Information': output}
         return {'message': 'User not found.'}, 404
-
-class ShowInformationUserEmail(Resource):
-    @jwt_required
-    def get(self, email):
-        person = PersonModel.find_by_email(email)
-        if person:
-            person_info = person.json()
-            crp = person.psychologists.crp
-            date_of_birth = person.psychologists.date_of_birth
-            password = person.psychologists.password
-
-            hospital = PersonModel.find_by_id(2)
-            registry_number = hospital.hospitals.registry_number
-            social_reason = hospital.hospitals.social_reason
-
-            output = {'Basic Informations': [person_info], 'Psychologist Information': {'crp': crp, 'date of birth': date_of_birth, 'password': password},
-            'Hospital': {'social_reason': social_reason, 'registry_number': registry_number}}
-
-            return {'User Information': output}
-        return {'message': 'Item not found.'}, 404
