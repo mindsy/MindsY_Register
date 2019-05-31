4c92b56e266d9f6a1784143129053ebe41b37166
Daniel Sousa <daniel_sousa.unb@outlook.com>
2019-05-31 16:35:24 -0300

    Corrigindo erros

--- resources/user_information.py
+++ resources/user_information.py
@@ -9,12 +9,19 @@ class ShowPsychologistInformationCRP(Resource):
         if psychologists:
             person_info = psychologists.PERSON.json()
             psychologists_info = psychologists.json()
-            hospital_info = psychologists.hospital_psychologists[0].HOSPITAL.json()
+
+            obj_ph = psychologists.hospital_psychologists.all()
+            hospital_info = None
+            flag = 0
+            for ph in obj_ph:
+                hospital_info = psychologists.hospital_psychologists[flag].HOSPITAL.json()
+                flag += 1
+
+            flag = None
 
             output = []
             psychologists_info.update({'person': person_info}),
             psychologists_info.update({'hospital': hospital_info})
             output.append(psychologists_info)
-
             return {"psychologist": output}
         return {'message': 'User not found.'}, 404
