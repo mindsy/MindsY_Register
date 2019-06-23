3f7884f846e5cf95c3e385e755c92099a6742407
Daniel Sousa <daniel_sousa.unb@outlook.com>
2019-06-23 20:17:49 -0300

    Adicionando teste na model person
    Co-authored-by: pedroMiranda7410 <pmiranda7410@gmail.com>
    Co-authored-by: deborapvilela <deborapvilela@gmail.com>

---
+++ tests/unit/models/test_person.py
@@ -0,0 +1,23 @@
+from models.person import PersonModel
+from tests.base_test import BaseTest
+
+
+class ItemTest(BaseTest):
+    def test_create_item(self):
+        person = PersonModel('test', 'test@test.com')
+
+        self.assertEqual(person.name, 'test',
+                         "The name of the item after creation does not equal the constructor argument.")
+        self.assertEqual(person.email, 'test@test.com',
+                         "The price of the item after creation does not equal the constructor argument.")
+
+    def test_item_json(self):
+        person = PersonModel('test', 'test@gmail.com')
+        expected ={
+            'id_person': None, 'name': 'test', 'email': 'test@gmail.com', 'telephone': []
+        }
+
+        self.assertEqual(
+            person.json(),
+            expected,
+            "The JSON export of the item is incorrect. Received {}, expected {}.".format(person.json(), expected))
